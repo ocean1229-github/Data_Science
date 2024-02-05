528f85fdd729c7bb5b667409e548cfd051516341
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2024-02-05 23:10:14 +0900

    numpy array tips

---
+++ Study/02_numpy_array_tips.py
@@ -0,0 +1,10 @@
+#tips
+import numpy as np
+array1 = np.full(6,0)
+array2 = np.zeros(6)
+array3 = np.full(6, 6)
+array4 = np.ones(9)
+array5 = np.random.random(6)
+array6 = np.random.random(10)
+
+import matplotlib.pyplot as plt
