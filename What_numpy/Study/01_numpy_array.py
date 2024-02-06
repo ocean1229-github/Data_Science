457f343cecc0c73a839b37a5fe16a692b321a86b
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2024-02-06 22:50:05 +0900

    numpy 개념 정리

---
+++ What_numpy/Study/01_numpy_array.py
@@ -0,0 +1,20 @@
+#google colab으로 진행해야하는데 로그인 인증 이슈로 그냥 여기 코드를 작성하겠음.
+import numpy
+
+array1 = numpy.array{[2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]}
+
+array1
+
+type(array1)
+
+array1.shape
+
+array2 = numpy.array{[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]}
+
+type(array2)
+
+array2.shape
+
+array1.size
+
+array2.size
