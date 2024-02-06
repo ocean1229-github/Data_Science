42bb31a3a5774689a24472fc8c2ea0b0ad7e7a37
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2024-02-06 22:50:22 +0900

    numpy indexing slicing

---
+++ Indexing/Study/01_numpy_math.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+array1 = np.arange(10)
+array2 = np.arange(10, 20)
+
+array1
+
+array2
+
+array1 * 2
+
+array1 / 2
+
+array1 + 2
+
+array1 ** 2
+
+array1 + array2
+array1 * array2
+array1 / array2
