7292a7670ee3f48ad519ed50e08d35ad62fee978
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2024-02-07 23:45:09 +0900

    read_csv

---
+++ What_pandas/Ex/02_read_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# 여기에 코드를 작성하세요
+df = pd.read_csv('data/popular_baby_names.csv')
+
+# 테스트 코드
+df
+
+
+import pandas as pd
+
+df = pd.read_csv('data/popular_baby_names.csv')
+df # 테스트 코드
---
+++ What_pandas/Ex/03_read_csv2.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+# 여기에 코드를 작성하세요
+df = pd.read_csv('data/mega_millions.csv', index_col = 0)
+
+# 테스트 코드
+df
