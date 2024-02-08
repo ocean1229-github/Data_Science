b218c3f11ff95a97bf266fa5243e3d5419f3b337
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2024-02-08 22:13:59 +0900

    pandas phone csv

--- What_pandas/Study/04_iphone.py
+++ What_pandas/Study/04_iphone.py
@@ -3,3 +3,15 @@ import pandas as pd
 iphone_df = pd.read_csv('iphone.csv', index_col = 0)
 iphone_df
 
+iphone_df.loc['iPhone 8', '메모리']
+
+iphone_df.loc['iPhone X', :]
+# 전체 데이터 가져옴
+
+iphone_df.loc['iPhone X']
+#위 코드랑 같음
+#타입은 시리즈
+
+type(iphone_df.loc['iPhone X'])
+
+iphone_df.loc[:, '출시일']
