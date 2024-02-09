2f289735b1b6dab70e9c5ebbdd0360c5490cd7d3
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2024-02-09 23:07:03 +0900

    DataFrame 비교, 조건

--- What_pandas/Ex/06_Broadcast2.py
+++ What_pandas/Ex/06_Broadcast2.py
@@ -10,8 +10,13 @@ newDF = df[trueDF]
 newDF['KBS']
 
 # 모범답안 
-import pandas as pd
-
-df = pd.read_csv('data/broadcast.csv', index_col=0)
 boolean_KBS = df['KBS'] > 30
 df.loc[boolean_KBS, 'KBS']
+
+
+
+win_chosun = df['TV CHOSUN'] > df['SBS']
+df.loc[win_chosun, ('SBS', 'TV CHOSUN')]
+
+# 모범답안
+df.loc[df['SBS'] < df['TV CHOSUN'], ['SBS', 'TV CHOSUN']]
