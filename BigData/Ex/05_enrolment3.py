4cb966f2a35b5af43ee4ea382e3d79be580049da
DoohyenKang <62789182+ocean1229-github@users.noreply.github.com>
2024-02-22 22:26:56 +0900

    빅데이터 다루기 문제 (많이 어려우니 복습 자주 할 것)

--- BigData/Ex/05_enrolment3.py
+++ BigData/Ex/05_enrolment3.py
@@ -3,30 +3,16 @@ import pandas as pd
 df = pd.read_csv('data/enrolment_3.csv')
 
 # 여기에 코드를 작성하세요
+room_list = df.loc[df['status'] == 'allowed', 'room assignment'].unique()
 
 # 조건 1
-sorted(df['room assignment'])
-auditorium_count = len(list(df['room assignment'] == 'Auditorium'].index))
-
-#"room number" 컬럼에는 Auditorium, Large room, Medium room, Small room, Not assigned 값들이 있음.
-#오름차순으로 리스트에 넣어줌
-df['room number'].value_counts().index
-room = list(df['room number'].value_counts().index)
-room.sort()
-
-#room number 컬럼에 '강의실종류 + 번호'방식으로 값들을 만들어 'course name'별로 할당시키기 위한 리스트
-room_detail = ['Auditorium-', 'Large-', 'Medium-', 'Small-']
-
-
-#방종류별로 거기에 맞는 강의종류들을 list로 뽑고, 알파벳 순서대로 '강의실종류 + 번호' 방식의 강의실 번호를 부여합니다.
-
-for i in range(4):
-    course_name_list = list(df.loc[df['room number'] == room[i], 'course name'].value_counts().index)
-    course_name_list.sort()
-    for j in range(len(course_name_list)):
-        df.loc[df['course name'] == course_name_list[j], 'room number'] = '%s%d' %(room_detail[i], j+1)
-df
-
+for room in room_list:
+    course_list = sorted(df.loc[df['room assignment'] == room, 'course name'].unique())
+    
+    i = 1
+    for course in course_list:
+        df.loc[(df['course name'] == course) & (df['status']=='allowed'), 'room assignment'] = room.split()[0] + '-' + str(i)
+        i += 1
 
 # 조건 2
 not_assigned = df['status'] == 'not allowed'
@@ -37,3 +23,39 @@ df.rename(columns = {'room assignment' : 'room number'}, inplace = True)
 
 # 테스트 코드
 df
+
+
+# 모범 답안
+import pandas as pd
+
+df = pd.read_csv('data/enrolment_3.csv')
+
+# 과목별 인원 가져오기
+allowed = df["status"] == "allowed"
+course_counts = df.loc[allowed, "course name"].value_counts()
+
+# 각 강의실 규모에 해당되는 과목 리스트 만들기
+auditorium_list = list(course_counts[course_counts >= 80].index)
+large_room_list = list(course_counts[(80 > course_counts) & (course_counts >= 40)].index)
+medium_room_list = list(course_counts[(40 > course_counts) & (course_counts >= 15)].index)
+small_room_list = list(course_counts[(15 > course_counts) & (course_counts > 4)].index)
+
+# 강의실 이름 붙이기
+for i in range(len(auditorium_list)):
+    df.loc[(df["course name"] == sorted(auditorium_list)[i]) & allowed, "room assignment"] = "Auditorium-" + str(i + 1)
+
+for i in range(len(large_room_list)):
+    df.loc[(df["course name"] == sorted(large_room_list)[i]) & allowed, "room assignment"] = "Large-" + str(i + 1)
+    
+for i in range(len(medium_room_list)):
+    df.loc[(df["course name"] == sorted(medium_room_list)[i]) & allowed, "room assignment"] = "Medium-" + str(i + 1)
+    
+for i in range(len(small_room_list)):
+    df.loc[(df["course name"] == sorted(small_room_list)[i]) & allowed, "room assignment"] = "Small-" + str(i + 1)
+
+# column 이름 바꾸기
+df.rename(columns={"room assignment": "room number"}, inplace = True)
+    
+# 테스트 코드
+df
+
